2ad3f7878dd35f0ee434ec65eb9203ede236f267
Rui Costa <ruicosta.pr@gmail.com>
2020-12-26 10:10:18 +0000

    gprs ready

--- gsm.py
+++ gsm.py
@@ -25,7 +25,12 @@ phone.setup()
 #phone.delete_storage_message(3, "ME")
 # phone.empty_storage_message("SM")
 
+joana = "+351919365209"
+filipe = "+351915884402"
+rui = "+351919873697"
+luisa = "+351915423077"
 
+meter = "+351962156457"
 serial = "156318"
 
 to = ""
